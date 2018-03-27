a61ac258ee1887f518244bd3d5bde94ad8c7103e
Valentin <lefebvre.valentin76@gmail.com>
2018-03-27 14:06:33 +0200

    Valentin-editer-collab

--- errors.js
+++ errors.js
@@ -1,5 +1,11 @@
 var url = location.search;
-if (url === '?errors') {
-  var errors = document.querySelectorAll('input, textarea');
-  errors.forEach(param => {param.classList.add('is-invalid')});
+if (url == '?errors') {
+  if (window.location.href.includes('creer')) {
+    var erreurs = document.querySelectorAll('input');
+    erreurs.forEach(param => {param.classList.add('is-invalid')});
+  }
+  if (window.location.href.includes('editer')) {
+    var erreurs = document.querySelectorAll('#accordion input');
+    erreurs.forEach(param => {param.classList.add('is-invalid')});
+  }
 }
