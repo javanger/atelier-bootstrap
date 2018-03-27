7b695ea6624659f7dc30bd2e5719d0fcc2ace872
Guillaume <guillaume.gobert@gmail.com>
2018-03-27 13:41:48 +0200

    Correction du script

--- error.js
+++ error.js
@@ -1,5 +1,17 @@
 var url = location.search;
 if (url == '?errors') {
-  var lesErrors = document.querySelectorAll('input');
+  if (window.location.href.includes('creer')) {
+    var lesErrors = document.querySelectorAll('input');
+    lesErrors.forEach(param => {param.classList.add('is-invalid')});
+    var lesErrors = document.querySelectorAll('select');
+    lesErrors.forEach(param => {param.classList.add('is-invalid')});
+  }
+  var lesErrors = document.querySelectorAll('#accordion input');
   lesErrors.forEach(param => {param.classList.add('is-invalid')});
+  var lesErrors = document.querySelectorAll('select');
+  lesErrors.forEach(param => {param.classList.add('is-invalid')});
+}
+else {
+  var lesErrors = document.querySelectorAll('#accordion input');
+  lesErrors.forEach(param => {param.classList.add()});
 }
