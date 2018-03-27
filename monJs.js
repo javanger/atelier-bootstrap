3cf90969522f0b5bb320a5c0385b946ee1b3ebf5
Emmanuel Chêne <emma.chene@gmail.com>
2018-03-27 15:13:04 +0200

    Ajout édition collb

--- monJs.js
+++ monJs.js
@@ -17,14 +17,11 @@
     }, false);
 })();
 
-
 var url = location.search;
 if (url === '?errors') {
     var errors = document.querySelectorAll('input, textarea');
     errors.forEach(param => { param.classList.add('is-invalid') });
 }
-
-
 
 $(document).ready(function () {
     $("#actionmodal").click(function () {
