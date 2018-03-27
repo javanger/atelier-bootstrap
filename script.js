4be86a8da337cf7efe1c6cd4f390d1c0b48fcbfc
fatima <fatima.elyousfi83@gmail.com>
2018-03-27 12:16:59 +0200

    editer collab

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ if (url == '?errors') {
 var lesErrors = document.querySelectorAll('input');
 lesErrors.forEach(param => {param.classList.add('is-invalid')});
 }
+form.addEventListener('submit', function (event) {
+  if (form.checkValidity() === false) {
+        event.preventDefault();
+            event.stopPropagation();
+       }
+        form.classList.add('was-validated');
+  }, false);
