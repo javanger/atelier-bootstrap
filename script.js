a973f946edfe21a5d24c119750f0b7460f2e19d5
fatima <fatima.elyousfi83@gmail.com>
2018-03-27 14:42:14 +0200

    autre modif

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ form.addEventListener('submit', function (event) {
        }
         form.classList.add('was-validated');
   }, false);
+
+
+
+  var Nom {
+    localStorage.setItem("Nom");
+    document.getElementById('Nom').value = Nom;
+  }
+
+  function save{
+sessionStorage.setItem('NOM', 'value')
+localStorage.setItem('Nom', JSON.stringify(Nom));
+}
+
+
+
+
+
+
+
+
+
+
+function save() {
+localStorage.setItem("Nom", "Nom");
+localStorage.setItem("Prénom", "Prénom");
+localStorage.setItem("Adresse", "Adresse");
+localStorage.setItem("Date", "Date");
+localStorage.setItem("numéro", "numéro");
+
+
+alert( "Nom = " + localStorage.getItem("Nom"));
+}
