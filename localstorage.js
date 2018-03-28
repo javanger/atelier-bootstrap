aa261bd64233ee91a0cbe4a090c8e73035ff2171
ange <abdjinan@yahoo.fr>
2018-03-28 09:33:50 +0200

    data

---
+++ localstorage.js
@@ -0,0 +1,11 @@
+// Détection du support
+if(typeof sessionStorage!='undefined') {
+    if('message' in sessionStorage) {
+      alert("Message récupéré");
+      document.getElementById('message').value = sessionStorage.getItem('message');
+    }
+  } else {
+    alert("sessionStorage n'est pas supporté");
+  }
+
+  
